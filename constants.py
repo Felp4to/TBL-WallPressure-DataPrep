96a3f6de4ac7b1f462e629fea2fd2aa28411075f
Felp4to <tardioli.paolo@gmail.com>
2025-02-10 17:59:50 +0100

    added read_channel.ipynb

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
 # constants.py 
 
-FOLDER_PATH = "../../Dataset NASA/B101/B101/Header.mat"
+HEADER_PATH = "../../Dataset NASA/B101/B101/Header.mat"
+CHANNEL_1_PATH = "../../Dataset NASA/B101/B101/Channel1.mat"
+CHANNEL_2_PATH = "../../Dataset NASA/B101/B101/Channel1.mat"
+
