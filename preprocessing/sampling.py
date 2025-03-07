781b3a13da8667ec2cdd40e8791163dd57c57f21
Felp4to <tardioli.paolo@gmail.com>
2025-03-07 14:41:21 +0100

    Added method slicing and sampling

--- preprocessing/sampling.py
+++ preprocessing/sampling.py
@@ -1,21 +1,28 @@
 # sampling.py
 
+import constants as cs
 import pandas as pd
 
 
+# extract first n seconds from time series
+def slicing(file_path, secs):
+    """Estrae i primi n secondi della serie temporale."""
+    df = pd.read_csv(file_path)
+    num_samples = int(secs * cs.SAMPLING_RATE)
+    return df[:num_samples]
 
 # sampling standard
-def sampling_csv(file_path, sampling_num):
-    df = pd.read_csv(file_path)
-    print_memory_usage(df)
-    return df.iloc[::sampling_num]
+def sampling_csv(data_df, sampling_factor):
+    #df = pd.read_csv(file_path)
+    #print_memory_usage(data_df)
+    return data_df.iloc[::sampling_factor]
 
 
 # sampling with average
-def sampling_avg_csv(file_path, sampling_num):
-    df = pd.read_csv(file_path)
-    df_reduced = df['singleData'].groupby(df.index // sampling_num).mean().reset_index(drop=True)
-    print_memory_usage(df_reduced)
+def sampling_avg_csv(data_df, sampling_factor):
+    #df = pd.read_csv(file_path)
+    df_reduced = data_df['singleData'].groupby(data_df.index // sampling_factor).mean().reset_index(drop=True)
+    #print_memory_usage(df_reduced)
     return df_reduced
 
 
